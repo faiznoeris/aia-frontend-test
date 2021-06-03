10c71deb276c1c57a60b516cb99d98f3505f03e9
faiznoeris <m.faiznoeris@gmail.com>
2021-06-03 12:38:00 +0700

    add search by tags

--- src/App.js
+++ src/App.js
@@ -11,10 +11,15 @@ function App() {
     const [total, setTotal] = useState(0)
     const [page, setPage] = useState(1)
     const [loading, setLoading] = useState(false)
+    const [tags, setTags] = useState('')
 
-    const getFeeds = (pageNumber = 1, limit = 1) =>
+    const getFeeds = (pageNumber = 1, limit = 1, tags = '') =>
         fetch(
-            `${process.env.REACT_APP_BACKEND_URL}?pageNumber=${pageNumber}&limit=${limit}`
+            `${
+                process.env.REACT_APP_BACKEND_URL
+            }?pageNumber=${pageNumber}&limit=${limit}${
+                tags ? `&tags=${tags}` : ''
+            }`
         )
             .then(resp => resp.json())
             .then(json => {
@@ -35,8 +40,14 @@ function App() {
     const handleChangePagination = (event, value) => {
         // default limit by only 1 image
         setLoading(true)
-        getFeeds(value, 1)
+        getFeeds(value, 1, tags)
         setPage(value)
+    }
+
+    const handleChangeSearch = e => {
+        const tags = e.target.value
+        getFeeds(page, 1, tags)
+        setTags(tags)
     }
 
     return (
@@ -50,6 +61,7 @@ function App() {
                     id='standard-search'
                     label='Search field'
                     type='search'
+                    onChange={handleChangeSearch}
                 />
                 <br />
                 {loading ? (
