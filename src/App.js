2aa80774222f2c6e280598eb2b04f388e1802b76
faiznoeris <m.faiznoeris@gmail.com>
2021-06-03 12:26:37 +0700

    add card with pagination and search box & removed removed bg color

--- src/App.js
+++ src/App.js
@@ -1,25 +1,65 @@
-import logo from './logo.svg';
-import './App.css';
+import { useEffect, useState } from 'react'
+import './App.css'
+import Card from './Card'
+import CssBaseline from '@material-ui/core/CssBaseline'
+import TextField from '@material-ui/core/TextField'
+import Pagination from '@material-ui/lab/Pagination'
 
 function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+    const [feeds, setFeeds] = useState([])
+    const [total, setTotal] = useState(0)
+    const [page, setPage] = useState(1)
+
+    const getFeeds = (pageNumber = 1, limit = 1) =>
+        fetch(
+            `http://localhost:4000/flickr-feeds?pageNumber=${pageNumber}&limit=${limit}`
+        )
+            .then(resp => resp.json())
+            .then(json => {
+                try {
+                    setFeeds(json.items)
+                    setTotal(json.total)
+                } catch (err) {
+                    console.log(err)
+                }
+            })
+
+    useEffect(() => {
+        getFeeds()
+    }, [])
+
+    const handleChangePagination = (event, value) => {
+        // default limit by only 1 image
+        getFeeds(value, 1)
+        setPage(value)
+    }
+
+    return (
+        <div className='App'>
+            <CssBaseline />
+
+            <header className='App-header'>
+                <p style={{ color: 'black' }}>Flickr Public Photos Feeds</p>
+
+                <TextField
+                    id='standard-search'
+                    label='Search field'
+                    type='search'
+                />
+                <br />
+                {feeds.map(val => (
+                    <Card imgData={val} />
+                ))}
+                <br />
+                <Pagination
+                    count={total}
+                    defaultPage={1}
+                    page={page}
+                    onChange={handleChangePagination}
+                />
+            </header>
+        </div>
+    )
 }
 
-export default App;
+export default App
---
+++ src/Card.js
@@ -0,0 +1,42 @@
+import React from 'react'
+import { makeStyles } from '@material-ui/core/styles'
+import Card from '@material-ui/core/Card'
+import CardActionArea from '@material-ui/core/CardActionArea'
+import CardContent from '@material-ui/core/CardContent'
+import Typography from '@material-ui/core/Typography'
+
+const useStyles = makeStyles({
+    root: {
+        maxWidth: 345,
+    },
+})
+
+export default function ImgMediaCard(props) {
+    const { imgData } = props
+    const classes = useStyles()
+
+    return (
+        <Card className={classes.root}>
+            <CardActionArea>
+                <CardContent>
+                    <Typography gutterBottom variant='h5' component='h2'>
+                        {imgData.title}
+                    </Typography>
+                    <Typography
+                        variant='body2'
+                        color='textSecondary'
+                        component='p'
+                    >
+                        <div
+                            dangerouslySetInnerHTML={{
+                                __html: imgData.description,
+                            }}
+                        />
+                    </Typography>
+                    <br />
+                    {imgData.tags.split(' ').map(val => `${val} `)}
+                </CardContent>
+            </CardActionArea>
+        </Card>
+    )
+}
