a376de02b832a2fc62209dc9d5e569c3fc85726c
faiznoeris <m.faiznoeris@gmail.com>
2021-06-03 12:27:06 +0700

    add loading spinner

--- src/App.js
+++ src/App.js
@@ -4,11 +4,13 @@ import Card from './Card'
 import CssBaseline from '@material-ui/core/CssBaseline'
 import TextField from '@material-ui/core/TextField'
 import Pagination from '@material-ui/lab/Pagination'
+import CircularProgress from '@material-ui/core/CircularProgress'
 
 function App() {
     const [feeds, setFeeds] = useState([])
     const [total, setTotal] = useState(0)
     const [page, setPage] = useState(1)
+    const [loading, setLoading] = useState(false)
 
     const getFeeds = (pageNumber = 1, limit = 1) =>
         fetch(
@@ -21,15 +23,19 @@ function App() {
                     setTotal(json.total)
                 } catch (err) {
                     console.log(err)
+                } finally {
+                    setLoading(false)
                 }
             })
 
     useEffect(() => {
+        setLoading(true)
         getFeeds()
     }, [])
 
     const handleChangePagination = (event, value) => {
         // default limit by only 1 image
+        setLoading(true)
         getFeeds(value, 1)
         setPage(value)
     }
@@ -47,9 +53,11 @@ function App() {
                     type='search'
                 />
                 <br />
-                {feeds.map(val => (
-                    <Card imgData={val} />
-                ))}
+                {loading ? (
+                    <CircularProgress />
+                ) : (
+                    feeds.map(val => <Card imgData={val} />)
+                )}
                 <br />
                 <Pagination
                     count={total}
